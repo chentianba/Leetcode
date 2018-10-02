1db40b92f9ff50628cecf0814b39402ddf799f5b
Tianba Chen <chentb666@gmail.com>
2018-10-02 15:55:45 +0800

    Update solution.h

--- Array/Easy-001.Two-Sum/solution.h
+++ Array/Easy-001.Two-Sum/solution.h
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> tm;
+        for(int i = 0; i < nums.size(); ++i) {
+            auto found = tm.find(nums[i]);
+            if (found != tm.end()) {
+                return {found->second, i};
+            }
+            tm.emplace(target - nums[i], i);
+        }
+        return {-1, -1};
+    }
+};
