477be99fb7d37e7ff83b39dcbf2331686b9bc299
Tianba Chen <chentb666@gmail.com>
2018-10-04 14:31:00 +0800

    Update solution.h

--- Array/Easy-027.Remove-Element/solution.h
+++ Array/Easy-027.Remove-Element/solution.h
@@ -1,1 +1,20 @@
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = nums.size() - 1;
+        int val_tmp;
+        for (int i = 0; i <= count; ++i) {
+            if (nums[i] == val) {
+                val_tmp = nums[i];
+                nums[i] = nums[count];
+                nums[count] = val_tmp;
+                i--;
+                count--;
+            }
+        }
+        return count + 1;
+    }
+};
